0ce2c03f121cbea84f82261a8725986cbb8c79a4
manohar <manumanohar1527@gmail.com>
2020-04-24 21:53:42 +0530

    initialised game setup

---
+++ app.js
@@ -0,0 +1,10 @@
+const score = document.querySelector('.score')
+const gameStart = document.querySelector(".gameStart");
+const gameArea = document.querySelector(".gameArea");
+
+//Welcome window
+gameStart.addEventListener('click',start);
+
+function start(){
+    console.log("Game started");
+}
