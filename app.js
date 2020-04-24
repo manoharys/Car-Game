9a08f62344b7f8733938a28e91e967c64972776a
manohar <manumanohar1527@gmail.com>
2020-04-24 22:38:38 +0530

    initialised requestAnimationFrame method

--- app.js
+++ app.js
@@ -2,31 +2,54 @@ const score = document.querySelector('.score')
 const gameStart = document.querySelector(".gameStart");
 const gameArea = document.querySelector(".gameArea");
 
+
+//Object which init game play
+let player = {};
+
+//Object which tracks the keyPresses
 let keys = {
-    ArrowTop:false,
-    ArrowRight:false,
-    ArrowDown:false,
-    ArrowLeft:false
+    ArrowTop: false,
+    ArrowRight: false,
+    ArrowDown: false,
+    ArrowLeft: false
 }
 
 //Welcome window
-gameStart.addEventListener('click',start);
+gameStart.addEventListener('click', start);
 
 //KeyPress Event
-window.document.addEventListener('keydown',pressOn);
-window.document.addEventListener('keyup',pressOff);
+window.document.addEventListener('keydown', pressOn);
+window.document.addEventListener('keyup', pressOff);
 
-//function which tracks the keypress events
-function pressOn(event){
-    //console.log(event.key);
-    keys[event.key] = true;
-    console.log(keys);
+//functions which tracks the keypress events
+{
+    function pressOn(event) {
+        keys[event.key] = true;
+        //console.log(keys);
+    }
+
+    function pressOff(event) {
+        keys[event.key] = false;
+        //console.log(keys);
+    }
 }
-function pressOff(event){
-   //console.log(event.key)
-   keys[event.key] = false;
-   console.log(keys);
-}
-function start(){
+
+//function which starts the game Play
+function start() {
     console.log("Game started");
+    gameArea.classList.remove('hide');
+    gameStart.classList.add('hide');
+    let car = document.createElement('div');
+    car.innerText = "CAR";
+    gameArea.appendChild(car);
+    player.start = true;
+    window.requestAnimationFrame(playGame);
+}
+
+//function 
+function playGame(){
+    if(player.start){
+        console.log("inplay")
+    }
+    window.requestAnimationFrame(playGame);
 }
